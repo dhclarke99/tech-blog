dcc86a76ed7a88434a8daebfd10d507be00b29ad
dhclarke99 <dhclarke99@gmail.com>
2023-05-11 21:11:08 -0400

    added homepage and main page html layout via handlebars

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -13,15 +13,15 @@ router.get('/', async (req, res) => {
         ],
       });
 
-      res.status(200).json(dbBlogData);
+    //   res.status(200).json(dbBlogData);
   
-    //   const blogs = dbBlogData.map((blog) =>
-    //     blog.get({ plain: true })
-    //   );
-    //   res.render('homepage', {
-    //     blogs,
-    //     loggedIn: req.session.loggedIn,
-    //   });
+      const blogs = dbBlogData.map((blog) =>
+        blog.get({ plain: true })
+      );
+      res.render('homepage', {
+        blogs,
+        // loggedIn: req.session.loggedIn,
+      });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
