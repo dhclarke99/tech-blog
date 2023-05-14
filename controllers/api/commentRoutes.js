55a733f0d3240824d5ae641469cdc262ce4ee0cc
dhclarke99 <dhclarke99@gmail.com>
2023-05-14 18:02:22 -0400

    created buttons for each blogpost in homepage and added user data

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+// *Checking get comments as a test NOT for users to do**
+router.get('/', async (req, res) => {
+    try {
+      const commentData = await Comment.findAll();
+  
+    //   req.session.save(() => {
+    //     req.session.user_id = userData.id;
+    //     req.session.logged_in = true;
+  
+        res.status(200).json(commentData);
+    //   });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
 module.exports = router;
