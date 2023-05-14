f381b92c9f703ff794904cd0a7ce17a8a90e3375
dhclarke99 <dhclarke99@gmail.com>
2023-05-13 22:26:31 -0400

    added login button and login.handlebars view

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -28,5 +28,15 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashBoard');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 
     module.exports = router;
