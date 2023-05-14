43d2569cf1c6b0e9d3560dbd574297629b2a217a
dhclarke99 <dhclarke99@gmail.com>
2023-05-14 19:36:07 -0400

    added functionality to post comment to a blogpost

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -26,4 +26,19 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.post('/', withAuth, async (req, res) => {
+    try {
+      
+      const commentData = await Comment.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(commentData);
+      res.render('/dashboard')
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
 module.exports = router;
--- public/js/blogPost.js
+++ public/js/blogPost.js
@@ -1,22 +1,25 @@
 const handleFormData = async (event) => {
     event.preventDefault();
     console.log("click");
-    const title = document.querySelector('.title').value;
-    const body = document.querySelector('.body').value;
+   
+    const body = document.querySelector('#body').value;
+    const blog_post_id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
     
-    console.log(title);
+    console.log(blog_post_id);
     console.log(body);
 
-    const response = await fetch('/api/blogs', {
+    const response = await fetch('/api/comments', {
         method: 'POST',
-        body: JSON.stringify({title, body}),
+        body: JSON.stringify({ body, blog_post_id}),
         headers: { 'Content-Type': 'application/json' },
       });
-      console.log(JSON.stringify({title}))
+      
       if (response.ok) {
       
         console.log("response worked")
-         document.location.replace('/dashboard');
+         document.location.replace(`/blog/${blog_post_id}`);
         
         } else {
         console.log("error")
@@ -27,53 +30,7 @@ const handleFormData = async (event) => {
     
 }
 
-const newCommentForm = async (event) => {
-event.preventDefault();
-console.log("click")
-// document.querySelector('#post-content').setAttribute("style", "visibility:visible")
-
-const parent = document.querySelector('#post-content');
-const div1 = document.createElement('div');
-const div2 = document.createElement('div');
-const div3 = document.createElement('div');
-const div4 = document.createElement('div');
-const h2 = document.createElement('h2');
-const h3One = document.createElement('h3');
-const h3Two = document.createElement('h3');
-const input1 = document.createElement('input');
-const input2 = document.createElement('input');
-const button = document.createElement('button');
-h2.innerText = "Create New Post";
-h3One.innerText = "Title";
-h3Two.innerText = "Content";
-button.innerText = "Submit";
-
-
-parent.appendChild(div1);
-div1.appendChild(h2);
-parent.appendChild(div2);
-div2.appendChild(h3One);
-div2.appendChild(input1);
-parent.appendChild(div3);
-div3.appendChild(h3Two);
-div3.appendChild(input2);
-parent.appendChild(div4);
-div4.appendChild(button);
-
-button.setAttribute("class", "submit");
-input1.setAttribute("class", "title");
-input2.setAttribute("class", "body");
-
 document
-.querySelector('.submit')
+.querySelector('#submit')
 .addEventListener('click', handleFormData);
-
-
-}
-
-
-console.log("listening")
-  document
-    .querySelector('#new-post')
-    .addEventListener('click', newCommentForm);
 
