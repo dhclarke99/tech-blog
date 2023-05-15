33893698d36ab1c1f245114a7f5b8dfc2d077119
dhclarke99 <dhclarke99@gmail.com>
2023-05-14 20:51:54 -0400

    created rough functionality for edit/delete post

--- public/js/dashBoard.js
+++ public/js/dashBoard.js
@@ -1,3 +1,73 @@
+
+const editPost = async (event) => {
+
+}
+
+const deletePost = async (event) => {
+  event.preventDefault();
+  const response = await fetch('/api/blogs/', {
+    method: 'POST',
+    body: JSON.stringify({title, body}),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  console.log(JSON.stringify({title}))
+  if (response.ok) {
+  
+    console.log("response worked")
+     document.location.replace('/dashboard');
+    
+    } else {
+    console.log("error")
+    alert(response.statusText);
+  }
+}
+
+
+const editOrDelete = async (event) => {
+  event.preventDefault();
+    console.log("click");
+    const parent = document.querySelector('.edit-delete');
+    
+    const editButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+    const blogId = event.target.id;
+    console.log(blogId);
+
+    editButton.innerText = "Edit Post";
+    deleteButton.innerText = "Delete Post";
+
+    editButton.setAttribute("id", "edit");
+    deleteButton.setAttribute("id", "delete");
+
+    parent.appendChild(editButton);
+    parent.appendChild(deleteButton);
+
+    const response = await fetch('/blog', {
+      method: 'POST',
+      body: JSON.stringify({ body, blog_post_id}),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    
+    if (response.ok) {
+    
+      console.log("response worked")
+       document.location.replace(`/blog/${blog_post_id}`);
+      
+      } else {
+      console.log("error")
+      alert(response.statusText);
+    }
+
+    document
+    .querySelector('#edit')
+    .addEventListener('click', editPost);
+
+    document
+    .querySelector('#delete')
+    .addEventListener('click', deletePost);
+  
+}
+
 const handleFormData = async (event) => {
     event.preventDefault();
     console.log("click");
@@ -30,7 +100,6 @@ const handleFormData = async (event) => {
 const newPostForm = async (event) => {
 event.preventDefault();
 console.log("click")
-// document.querySelector('#post-content').setAttribute("style", "visibility:visible")
 
 const parent = document.querySelector('#post-content');
 const div1 = document.createElement('div');
@@ -76,3 +145,7 @@ console.log("listening")
   document
     .querySelector('#new-post')
     .addEventListener('click', newPostForm);
+
+    document.querySelectorAll('.old-post').addEventListener('click', () => {
+      editOrDelete;
+    });
