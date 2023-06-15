17d48e2dd7279b3b04811a053378cc950b3f9daf
dhclarke99 <dhclarke99@gmail.com>
2023-06-14 21:25:06 -0400

    updated blogpost model to format timestamp to mm/dd/yyyy

--- models/blogpost.js
+++ models/blogpost.js
@@ -1,45 +1,48 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
-class BlogPost extends Model {
-
-}
+class BlogPost extends Model {}
 
 BlogPost.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        title: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        body: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model:'user',
-                key: 'id',
-            }
-
-        }
-
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-
-        sequelize,
-        timestamps: true,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'blogPost',
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      }
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      get() {
+        // Format the timestamp to "mm/dd/yyyy" when accessed
+        const date = this.getDataValue('createdAt');
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+      }
     }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blogPost',
+  }
 );
 
 module.exports = BlogPost;
